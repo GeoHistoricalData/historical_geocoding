3cbe8435c1af6fbfcbc92cc0d56a4e46aa70c7ee
Remi-C <remi.cura@gmail.com>
2017-01-17 16:21:46 +0100

    example file to create a geocoding user, which is necessary for rest acess. password has been removed from declaration

---
+++ example/example_rest/creating_a_user_role_and_conf_it.sql
@@ -0,0 +1,14 @@
+﻿----------
+-- Remi Cura, copyright 2017 , belle epoque, IGN 
+--
+-- Example of how to set up a geocoding user role with adequate privileges
+
+
+CREATE ROLE geocoding_user LOGIN  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION CONNECTION LIMIT 10;
+COMMENT ON ROLE geocoding_user IS 'user for geocoding, has almost no right except using the correct functions';
+
+
+GRANT USAGE ON SCHEMA public,geohistorical_object,historical_geocoding TO geocoding_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA public, geohistorical_object,historical_geocoding TO geocoding_user;
+GRANT EXECUTE  ON ALL FUNCTIONS IN SCHEMA public, geohistorical_object,historical_geocoding  TO geocoding_user;
+
