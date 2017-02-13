7015ba68a0f86947540882c7a1afd7b49409f05f
Remi-C <remi.cura@gmail.com>
2017-02-13 11:21:48 +0100

    website: interactive : first try to add a form to edit adresse point properties

--- example/example_rest/interactive_geocoding/Leaflet-WFST/examples/geocoding_form.js
+++ example/example_rest/interactive_geocoding/Leaflet-WFST/examples/geocoding_form.js
@@ -4,92 +4,89 @@
  */ 
 
 
-//  var showSidebar = function (marker) {
-//    sidebar._marker = marker;
-//
-//    if (sidebar._marker.feature) {
-//      document.getElementById('idTextbox').value = sidebar._marker.feature.id || '';
-//
-//      if (sidebar._marker.feature.properties) {
-//        document.getElementById('osmidTextbox').value = sidebar._marker.getProperty('osm_id') || '';
-//        document.getElementById('manmadeTextbox').value = sidebar._marker.getProperty('man_made') || '';
-//        document.getElementById('nameTextbox').value = sidebar._marker.getProperty('name') || '';
-//        document.getElementById('amenityTextbox').value = sidebar._marker.getProperty('amenity') || '';
-//        document.getElementById('leisureTextbox').value = sidebar._marker.getProperty('leisure') || '';
-//        document.getElementById('officeTextbox').value = sidebar._marker.getProperty('office') || '';
-//        document.getElementById('shopTextbox').value = sidebar._marker.getProperty('shop') || '';
-//        document.getElementById('sportTextbox').value = sidebar._marker.getProperty('sport') || '';
-//        document.getElementById('tourismTextbox').value = sidebar._marker.getProperty('tourism') || '';
-//      }
-//
-//      sidebar._onApplyBtnClick = function () {
-//        if (!sidebar._marker) {
-//          return;
-//        }
-//
-//        sidebar._marker.setProperties(sidebar._marker.feature.properties || {});
-//
-//        var propertiesChanged = false;
-//
-//        var applyPropertyValue = function (property, value) {
-//          if ((property || value) && property !== value) {
-//            property = value;
-//            propertiesChanged = true;
-//          }
-//        };
-//
-//        sidebar._marker.setProperties({
-//          osm_id:  document.getElementById('osmidTextbox').value || null,
-//          man_made:  document.getElementById('manmadeTextbox').value || null,
-//          name:  document.getElementById('nameTextbox').value || null,
-//          amenity:  document.getElementById('amenityTextbox').value || null,
-//          leisure:  document.getElementById('leisureTextbox').value || null,
-//          office:  document.getElementById('officeTextbox').value || null,
-//          shop:  document.getElementById('shopTextbox').value || null,
-//          sport:  document.getElementById('sportTextbox').value || null,
-//          tourism:  document.getElementById('tourismTextbox').value || null
-//        })
-//
-//        if (propertiesChanged) {
-//          sidebar._marker.fire('marker:edited');
-//        }
-//      };
-//
-//      var applyBtn = document.getElementById('applyButton');
-//      L.DomEvent.on(applyBtn, 'click', sidebar._onApplyBtnClick);
-//    }
-//
-//    sidebar.show();
-//  };
+  var showSidebar = function (marker) {
+    sidebar._marker = marker;
+
+    if (sidebar._marker.feature) {
+      document.getElementById('idTextbox').value = sidebar._marker.feature.id || '';
+
+      if (sidebar._marker.feature.properties) { //filling the form with values
+        document.getElementById('hname').value = sidebar._marker.getProperty('historical_name') || '';
+        document.getElementById('nname').value = sidebar._marker.getProperty('normalised_name') || ''; 
+        document.getElementById('fdate').value = 0 ; //sidebar._marker.getProperty('normalised_name') || ''; 
+        // @FIXME @DEBUG @ERROR //missing value returned from database 
+        document.getElementById('hsource').value = sidebar._marker.getProperty('historical_source') || '';
+        document.getElementById('nsource').value = sidebar._marker.getProperty('numerical_origin_process') || '';
+        document.getElementById('adist').value = sidebar._marker.getProperty('aggregated_distance') || '';
+        document.getElementById('sprecision').value = sidebar._marker.getProperty('spatial_precision') || '';
+        document.getElementById('cresult').value = sidebar._marker.getProperty('confidence_in_result') || '';
+        document.getElementById('sdist').value = sidebar._marker.getProperty('semantic_distance') || '';
+        document.getElementById('tdist').value = sidebar._marker.getProperty('temporal_distance') || '';
+        document.getElementById('ndist').value = sidebar._marker.getProperty('number_distance') || ''; 
+      }
+
+      sidebar._onApplyBtnClick = function () {
+        if (!sidebar._marker) {
+          return;
+        }
+
+        sidebar._marker.setProperties(sidebar._marker.feature.properties || {});
+
+        var propertiesChanged = false;
+
+        var applyPropertyValue = function (property, value) {
+          if ((property || value) && property !== value) {
+            property = value;
+            propertiesChanged = true;
+          }
+        };
+
+        sidebar._marker.setProperties({
+          historical_name:  document.getElementById('hname').value || null,
+          normalised_name:  document.getElementById('nname').value || null,
+          //fuzzy_date:  document.getElementById('fdate').value || null,//@FIXME  
+        })
+
+        if (propertiesChanged) {
+          sidebar._marker.fire('marker:edited');
+        }
+      };
+
+      var applyBtn = document.getElementById('applyButton');
+      L.DomEvent.on(applyBtn, 'click', sidebar._onApplyBtnClick);
+    }
+
+    sidebar.show();
+  };
   
   
 
-//  var hideSidebar = function () {
-//    if (sidebar._marker && sidebar._marker._popupToolbar && sidebar._marker._popupToolbar._removeToolbar) {
-//      sidebar._marker._popupToolbar._removeToolbar();
-//    }
-//
-//    var applyBtn = document.getElementById('applyButton');
-//
-//    if (sidebar._onApplyBtnClick) {
-//      L.DomEvent.off(applyBtn, 'click', sidebar._onApplyBtnClick);
-//    }
-//
-//    sidebar._marker = undefined;
-//    sidebar._onApplyBtnClick = undefined;
-//    sidebar.hide();
-//  };
-//
-//  L.DomEvent.on(sidebar.getCloseButton(), 'click', function () {
-//    hideSidebar();
-//  });
+  var hideSidebar = function () {
+    if (sidebar._marker && sidebar._marker._popupToolbar && sidebar._marker._popupToolbar._removeToolbar) {
+      sidebar._marker._popupToolbar._removeToolbar();
+    }
+
+    var applyBtn = document.getElementById('applyButton');
+
+    if (sidebar._onApplyBtnClick) {
+      L.DomEvent.off(applyBtn, 'click', sidebar._onApplyBtnClick);
+    }
+
+    sidebar._marker = undefined;
+    sidebar._onApplyBtnClick = undefined;
+    sidebar.hide();
+  };
+
+  L.DomEvent.on(sidebar.getCloseButton(), 'click', function () {
+    hideSidebar();
+  });
 
 
 
-//    marker.on('popuptoolbar:shown', function () {
-//      showSidebar(marker);
-//    });
+    marker.on('popuptoolbar:shown', function () {
+      showSidebar(marker);
+    });
 
-//    marker.on('popuptoolbar:closed', function () {
-//      hideSidebar();
-//    });
+    marker.on('popuptoolbar:closed', function () {
+      hideSidebar();
+    });
